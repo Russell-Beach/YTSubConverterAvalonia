60a8553d3d1ac394b18b94596734f1c0f49d09a7
Russell Beach <beachrussellr@gmail.com>
2026-04-02 02:50:28 -0400

    Improved drag and drop to only get first instance of a supported file type

--- YTSubConverterAvalonia/Views/MainWindow.axaml.cs
+++ YTSubConverterAvalonia/Views/MainWindow.axaml.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Platform.Storage;
 using CommunityToolkit.Mvvm.Messaging;
 using YTSubConverterAvalonia.ViewModels;
 
@@ -8,6 +12,11 @@ namespace YTSubConverterAvalonia.Views;
 
 public partial class MainWindow : Window
 {
+    private static readonly HashSet<string> SupportedSubtitleExtensions = new(StringComparer.OrdinalIgnoreCase)
+    {
+        ".ass", ".sbv", ".ytt", ".srv3", ".ttml", ".xml", ".dfxp"
+    };
+
     public MainWindow()
     {
         InitializeComponent();
@@ -20,19 +29,26 @@ public partial class MainWindow : Window
 
     private static void DragOver(object? sender, DragEventArgs e)
     {
-        // TODO: make this only register copy effect for the file formats we actually want
-        e.DragEffects = e.DataTransfer.Contains(DataFormat.File)
+        e.DragEffects = GetFirstSupportedFile(e.DataTransfer) is not null
             ? DragDropEffects.Copy
             : DragDropEffects.None;
     }
 
     private void Drop(object? sender, DragEventArgs e)
     {
-        var firstFile = e.DataTransfer.TryGetFiles()?.FirstOrDefault();
+        var firstFile = GetFirstSupportedFile(e.DataTransfer);
         if (firstFile is not null && DataContext is MainWindowViewModel mainWindow)
         {
             mainWindow.LoadFile(firstFile.Path.LocalPath);
         }
+    }
+
+    private static IStorageItem? GetFirstSupportedFile(IDataTransfer dataTransfer)
+    {
+        if (!dataTransfer.Contains(DataFormat.File)) return null;
+
+        return dataTransfer.TryGetFiles()?.FirstOrDefault(file =>
+            SupportedSubtitleExtensions.Contains(Path.GetExtension(file.Path.LocalPath)));
     }
 
     protected override void OnClosing(WindowClosingEventArgs e)

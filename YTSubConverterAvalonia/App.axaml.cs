0aea59936aa3757a30703e461c43ac9168acd062
Russell Beach <beachrussellr@gmail.com>
2026-04-19 14:01:49 -0400

    Avalonia 12 Update: Removed WebViewControl-Avalonia package in exchange for Avalonia.Controls.WebView Cleaned up some breaking changes from Avalonia 12 Upgrade
    Since Native Web View is currently not supported by Linux, and WebViewControl-Avalonia is not supported by Avalonia 12, I'll just leave this in a separate branch until something changes between those two and fix later.

--- YTSubConverterAvalonia/App.axaml.cs
+++ YTSubConverterAvalonia/App.axaml.cs
@@ -1,7 +1,5 @@
-using System.Linq;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using YTSubConverterAvalonia.Services;
 using YTSubConverterAvalonia.ViewModels;
@@ -22,10 +20,6 @@ public class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
-
             _mainWindow = new MainWindow();
             var viewModel = new MainWindowViewModel(new AvaloniaFileDialogService(_mainWindow));
             
@@ -48,15 +42,5 @@ public class App : Application
         _mainWindow.Show();
         _mainWindow.WindowState = Avalonia.Controls.WindowState.Normal;
         _mainWindow.Activate();
-    }
-
-    private void DisableAvaloniaDataAnnotationValidation()
-    {
-        // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
-            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
-
-        // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove) BindingPlugins.DataValidators.Remove(plugin);
     }
 }
